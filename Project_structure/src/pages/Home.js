35b73267c35656cd851eaaa2a0f2fc507ac20053
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2024-12-11 11:22:26 +0530

    Update and rename Home.js.txt to Home.js

---
+++ Project_structure/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Feed from '../components/Feed/Feed';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Social Media App</h1>
+      <Feed />
+    </div>
+  );
+};
+
+export default Home;
